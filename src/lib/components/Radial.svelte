685c3e2d713a53dc37b06091ec53ec49b31d1321
mradigen <55953083+mradigen@users.noreply.github.com>
2024-01-05 16:18:37 +0530

    chore: Adding UI components

---
+++ src/lib/components/Radial.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let progress = 0; // Progress value passed as a prop
+
+	// Calculate the stroke properties
+	const radius = 20; // Radius of the circle
+	const circumference = 2 * Math.PI * radius;
+	let strokeDashoffset = circumference - (progress / 100) * circumference;
+	let strokeColor = progress > 40 ? 'green' : 'red';
+</script>
+
+<svg width="50" height="50">
+	<circle
+		class="progress-circle"
+		stroke={strokeColor}
+		stroke-width="5"
+		fill="transparent"
+		r={radius}
+		cx="25"
+		cy="25"
+		stroke-dasharray={circumference}
+		stroke-dashoffset={strokeDashoffset}
+	/>
+</svg>
+
+<style>
+	.progress-circle {
+		transform: rotate(-90deg);
+		transform-origin: 50% 50%;
+		transition:
+			stroke-dashoffset 0.6s ease,
+			stroke 0.6s ease;
+	}
+</style>
---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let charities = [];
+
+	let loading = true;
+
+	onMount(async () => {
+		charities = await (await fetch('http://na.tripe.one:7777/news')).json();
+		loading = false;
+	});
+</script>
+
+<div class="mt-14 mx-[20%]">
+	<div class="p-4 text-lg font-semibold text-center">
+		Discover a selection of eco-conscious charities committed to environmental care and sustainable
+		development scraped from all over the internet. Explore their missions, projects, and contribute
+		to building a greener, healthier planet.
+	</div>
+	{#each charities as charity}
+		<div class="p-2">
+			<div
+				class=" text-green-100 bg-opacity-15 p-4 flex flex-col rounded-2xl border-primary border-2 relative"
+			>
+				<div class="text-2xl font-semibold">{name}</div>
+				<div>{charity.desc}</div>
+				<div
+					class="mt-2 bg-accent bg-opacity-25 border-2 border-primary w-fit p-2 rounded-xl text-sm font-semibold hover:scale-105 ml-auto"
+				>
+					<a href={link}>Read More</a>
+				</div>
+			</div>
+		</div>
+	{/each}
+</div>
