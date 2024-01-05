03b6ad93b4f4548cf66cdd5206707560629a8321
mradigen <55953083+mradigen@users.noreply.github.com>
2024-01-05 12:00:46 +0530

    feat: Charities UI

--- src/lib/components/Charity.svelte
+++ src/lib/components/Charity.svelte
@@ -2,10 +2,13 @@
 	export let name: string;
 	export let description: string;
 	export let link: string;
+	export let progress: number;
 </script>
 
 <div class="p-2">
-	<div class="bg-accent bg-opacity-15 p-4 flex flex-col rounded-2xl border-primary border-2">
+	<div
+		class="bg-accent bg-opacity-15 p-4 flex flex-col rounded-2xl border-primary border-2 relative"
+	>
 		<div class="text-2xl font-semibold">{name}</div>
 		<div>{description}</div>
 		<div
@@ -13,5 +16,10 @@
 		>
 			<a href={link}>Read More</a>
 		</div>
+		<progress
+			class="progress w-[75%] absolute h-full w-full top-0 left-0 z-[-1]"
+			value={progress}
+			max="100"
+		></progress>
 	</div>
 </div>
--- src/routes/green-charities/+page.svelte
+++ src/routes/green-charities/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import Charity from '$lib/components/Charity.svelte';
-	import charities from './charities.json';
+	// import charities from './charities.json';
+	import { onMount } from 'svelte';
+
+	let charities = [];
+
+	let loading = true;
+
+	onMount(async () => {
+		charities = await (await fetch('http://na.tripe.one:7777/charities')).json();
+		loading = false;
+	});
 </script>
 
 <div class="mt-14 mx-[20%]">
@@ -10,6 +20,11 @@
 		to building a greener, healthier planet.
 	</div>
 	{#each charities as charity}
-		<Charity name={charity.name} description={charity.description} link={charity.link} />
+		<Charity
+			name={charity.name}
+			description={charity.desc}
+			link={charity.url}
+			progress={charity.progress}
+		/>
 	{/each}
 </div>
