a002cadd09dd308a83e7296815006fc885dc20ac
mradigen <55953083+mradigen@users.noreply.github.com>
2024-01-05 16:32:12 +0530

    chore: Adding UI components

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -3,11 +3,8 @@
 
 	let charities = [];
 
-	let loading = true;
-
 	onMount(async () => {
 		charities = await (await fetch('http://na.tripe.one:7777/news')).json();
-		loading = false;
 	});
 </script>
 
@@ -20,14 +17,14 @@
 	{#each charities as charity}
 		<div class="p-2">
 			<div
-				class=" text-green-100 bg-opacity-15 p-4 flex flex-col rounded-2xl border-primary border-2 relative"
+				class="bg-accent text-black bg-opacity-15 p-4 flex flex-col rounded-2xl border-primary border-2 relative"
 			>
-				<div class="text-2xl font-semibold">{name}</div>
-				<div>{charity.desc}</div>
+				<div class="text-2xl font-semibold">{charity.title}</div>
+				<div>{charity.description}</div>
 				<div
 					class="mt-2 bg-accent bg-opacity-25 border-2 border-primary w-fit p-2 rounded-xl text-sm font-semibold hover:scale-105 ml-auto"
 				>
-					<a href={link}>Read More</a>
+					<a href={charity.url}>Read More</a>
 				</div>
 			</div>
 		</div>
