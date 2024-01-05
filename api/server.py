4b806bd563e126dd65e3963a6e48ef9e9aa8a144
mradigen <55953083+mradigen@users.noreply.github.com>
2024-01-05 09:23:05 +0530

    chore: Disable cors

--- api/server.py
+++ api/server.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from fastapi import FastAPI
 from supabase import create_client, Client
@@ -10,6 +11,17 @@ supabase: Client = create_client(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYmF1cWdzb2x4dnlhbWRkcGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzNzc4OTcsImV4cCI6MjAxOTk1Mzg5N30.hJx5GMmCS6PWnRio9hOJB04ZkoO5Cf-WD_O1zjc7yRI",
 )
 app = FastAPI()
+
+
+# Configure CORS to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from any origin
+    # Allow sending credentials (cookies, etc.) with requests
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 @app.get("/")
